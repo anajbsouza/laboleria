d76f9daa40a5f937eb57041fe63d7cda4676fed1
anajbsouza <anaju.batsouza@gmail.com>
2023-08-25 12:58:45 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.routes.js";
 
-const app = express()
-app.use(express.json())
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 5040
-app.listen(port, () => {
-	console.log(`Servidor rodando na porta ${port}`)
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 5040;
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
